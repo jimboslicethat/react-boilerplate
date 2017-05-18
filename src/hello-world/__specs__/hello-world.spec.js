c705d2d9d90d463e6653217469b44dd9a9233a0e
James Walsh <jamesandersonwalsh@gmail.com>
2017-05-17 22:09:21 -0600

    Upgrade all npm packages
    With exception to webpack, sinon, and broken postcss-loader

--- src/hello-world/__specs__/hello-world.spec.js
+++ src/hello-world/__specs__/hello-world.spec.js
@@ -1,6 +1,6 @@
 
 describe('hello-world', () => {
   it('should have mocha perform a bogus test', () => {
-    expect(2 === 2).to.be.true // eslint-disable-line no-self-compare
+    expect(true).to.not.be.false
   })
 })
